77559164067ed6d28a8b5acbf3781056e4cdaa46
victor <vitords1082@gmail.com>
2022-05-16 12:13:30 -0300

    commit "master"

--- passenger counter app/index.js
+++ passenger counter app/index.js
@@ -1,1 +1,15 @@
-//document.getElementById("count-el").innerText = 5
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflet the new count 
+let count = 0
+let countEl = document.getElementById("count-el") // passing argument in ()
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count 
+}
+
+function save() {
+    console.log(count);
+}
